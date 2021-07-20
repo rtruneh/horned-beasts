c49995fb9f473ad4f3e32fdacc6ed1a9aadf8c4b
rtruneh <rosa.truneh@gmail.com>
2021-07-19 20:50:50 -0500

    worked on lab2 functionality

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,26 +1,44 @@
   
 import React from 'react';
 
-import HornedBeast from './HornedBeast';
-import Black from '../img/hb2.jpg';
-import Gazelle from '../img/hb1.jpg';
+// import HornedBeast from './HornedBeast';
+// import Black from '../img/hb2.jpg';
+// import Gazelle from '../img/hb1.jpg';
 
-function Main() {
-    return (
-      <>
-        <HornedBeast 
-          title="Horned Animal" 
-          imgurl={Black} 
-          desc="black beauty"
+import {animalData} from '../data';
+
+export const Main = () =>{
+    return {
+        <>
+            {animalData.map((data, key) => {
+          return(
+          <div key={key}>
+              {data.image_url + 
+              " , " + 
+              data.title + 
+              "," + 
+              data.description})
+          </div>
+    };
+})}
+</>
+);
+};
+
+        /* <HornedBeast 
+        //   title="Horned Animal" 
+        //   imgurl={Black} 
+        //   desc="black beauty"
+        title="Horned Animal" 
+          image_url={Black} 
+          description="black beauty"
         />
         <HornedBeast 
           title="Gazelle" 
           imgurl={Gazelle} 
         />
-      </>
-      );
-    }
+      */
+  
 
 export default Main;
 
-//test//
