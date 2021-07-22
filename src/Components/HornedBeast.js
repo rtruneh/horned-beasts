081eaae620ae67bd7ce68adddd8a5529d462b22a
rtruneh <rosa.truneh@gmail.com>
2021-07-21 19:38:33 -0500

    button that tallies per click

--- src/Components/HornedBeast.js
+++ src/Components/HornedBeast.js
@@ -1,14 +1,29 @@
 import React from 'react';
 // import Black from '../img/hb2.jpg';
+//another way to import could also use import/from
+//useState: just render one piece of code
+const { useState } = React
+
 
 function HornedBeast({image_url, title, description}){
+    //function to give the button a job
+    const handleClick = () =>{
+        setCount(count+1);
+    }
+    //usestate(sets beg value)
+    const [count, setCount] = useState(0)
+
     return(
         <>
         <h2>{title}</h2>
         <img height = '500px' src={image_url} alt="Goat" title="Goat"/>
         <p>{description}</p>
+
+        <button onClick={handleClick}> Favorite {count}</button>
         </>
     )
 }
+
+
 
 export default HornedBeast;
